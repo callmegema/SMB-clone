bc7f99e51551fc8e28e1fdbb45a214b8375a614a
callmegema <sufyl1016@gmail.com>
2021-10-09 14:01:13 +0100

    fix aspect ratio

--- Assets/Scripts/MainCameraAspectRatio.cs
+++ Assets/Scripts/MainCameraAspectRatio.cs
@@ -12,23 +12,23 @@ public class MainCameraAspectRatio : MonoBehaviour {
 
 		Camera camera = GetComponent<Camera>();
 
-		if (scaleheight < 1.0f) {
-			Rect rect = camera.rect;
-			rect.width = 1.0f;
-			rect.height = scaleheight;
-			rect.x = 0;
-			rect.y = (1.0f - scaleheight) / 2.0f;
-			camera.rect = rect;
+		// if (scaleheight < 1.0f) {
+		// 	Rect rect = camera.rect;
+		// 	rect.width = 1.0f;
+		// 	rect.height = scaleheight;
+		// 	rect.x = 0;
+		// 	rect.y = (1.0f - scaleheight) / 2.0f;
+		// 	camera.rect = rect;
 
-		} else {
-			float scalewidth = 1.0f / scaleheight;
-			Rect rect = camera.rect;
-			rect.width = scalewidth;
-			rect.height = 1.0f;
-			rect.x = (1.0f - scalewidth) / 2.0f;
-			rect.y = 0;
-			camera.rect = rect;
-		}
+		// } else {
+		// 	float scalewidth = 1.0f / scaleheight;
+		// 	Rect rect = camera.rect;
+		// 	rect.width = scalewidth;
+		// 	rect.height = 1.0f;
+		// 	rect.x = (1.0f - scalewidth) / 2.0f;
+		// 	rect.y = 0;
+		// 	camera.rect = rect;
+		// }
 
 	}
 }
